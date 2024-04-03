d2dd0c6cdc80080426e312acf6131916077dce4b
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-04-03 12:32:21 -0300

    first commit

---
+++ src/HooksApp.jsx
@@ -0,0 +1,19 @@
+import { useState,useEffect } from "react"
+
+export const HooksApp = () => {
+  
+    useEffect( ()=>{
+
+    },[])
+  
+    return (
+    
+    <>
+
+     <h1>Hola Mundo</h1>  
+    
+
+    </>
+  
+  )
+}
